7411cc965e52f1597994fe4c90a865652923068e
michel <michelcaussade@gmail.com>
2021-06-05 11:39:46 -0400

    until now everything works and temperatures are now decimal

--- update.js
+++ update.js
@@ -7,15 +7,16 @@ async function Data(string, list){
     if(string === 'Add City'){
         const add = await AddCity()
         const city = add['Add']
-        const Temp = Math.floor((Math.random()*30) + 1)
-        const Max = Math.floor((Math.random()*30) + 1)
-        const Min = Math.floor((Math.random()*30) + 1)
+        const Temp = Math.floor((Math.random()*30) + 1) + (Math.random()*1 + 0);
+        const Max = Math.floor((Math.random()*30) + 1) + (Math.random()*1 + 0);
+        const Min = Math.floor((Math.random()*30) + 1) + (Math.random()*1 + 0);
         MainList[0].push(city)
-        MainList[1].push(Temp)
-        MainList[2].push(Max)
-        MainList[3].push(Min)
+        MainList[1].push(Temp.toFixed(1))
+        MainList[2].push(Max.toFixed(1))
+        MainList[3].push(Min.toFixed(1))
         return list
     }
+
 }
 
 
